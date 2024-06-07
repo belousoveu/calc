62bd8680d56aa0d7c1874481ee7c22b2299e8278
Belousoveu <belousov.eu@gmail.com>
2024-06-07 15:57:06 +0300

    step1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+String expression = new Scanner(System.in).nextLine();
+byte operation=getOperation(expression);
+    }
+
+    private static byte getOperation(String expression) {
+    byte result=0;
+    if (expression.indexOf('+')!=-1) {
+        result=1;
+    }
+    return result;
+
     }
 }
